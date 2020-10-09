e647f8ec1a55be1c5e11f7a096d841afd1e46e49
Alexander Bowers <7319abowers@gmail.com>
2020-10-09 22:48:48 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,9 +2,8 @@ def mario
   phrase ="It's-a me, Mario!"
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
-  status
 end
 
-def toadstool(status)
+def toadstool
   puts status
 end
